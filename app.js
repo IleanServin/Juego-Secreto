c5dddc509770492a781c1f69905435d2c8f96b58
IleanServin <135076520+IleanServin@users.noreply.github.com>
2024-01-23 20:07:04 -0600

    Version Final Juego

---
+++ app.js
@@ -0,0 +1,68 @@
+let numeroSecreto = 0;
+let intentos = 0 ;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento,texto) {
+    let elementoHTMl = document.querySelector(elemento);
+    elementoHTMl.innerHTML = texto;
+    return;
+}
+function  verificarIntento() {
+    let numeroDeUsuario =  parseInt(document.getElementById(`valorUsuario`).value);
+  
+    if( numeroDeUsuario === numeroSecreto){
+        asignarTextoElemento(`p`,`Acertaste el numero en ${intentos} ${(intentos === 1) ? `vez` : `veces`}`);
+        document.getElementById(`reiniciar`).removeAttribute(`disabled`);
+    
+    }   else {
+            if(numeroDeUsuario > numeroSecreto){
+                asignarTextoElemento(`p`,`El numero secreto es menor`);   
+            } else asignarTextoElemento(`p`, `El numero secreto es mayor`);
+            
+    }
+    intentos++;
+    limpiarCaja();
+    
+
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector (`#valorUsuario`).value = ``;
+    
+
+}
+
+function generarNumeroSecreto(){
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+//Si el numero generado esta incluido en la lista
+if(listaNumerosSorteados.includes(numeroGenerado)){
+    return generarNumeroSecreto();
+}else{
+listaNumerosSorteados.push(numeroGenerado);
+    return numeroGenerado;
+}
+
+}
+function condicionesIniciales(){
+    asignarTextoElemento(`h1`,`Juego Secreto!`);
+    asignarTextoElemento(`p`,`Indica un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    //limpiar la caja
+    limpiarCaja();
+    //Indicar mensaje de inicio
+    condicionesIniciales();
+    //generar el numero aleatorio
+    //desabilitar el boton de nuevo juego
+    document.querySelector(`#reiniciar`).setAttribute(`disabled`,`true`);
+    //Reiniciar el numero de intentos
+    
+
+}
+condicionesIniciales();
+
